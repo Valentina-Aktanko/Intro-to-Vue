efc0fa51e7fa07d0454536533252ac7f88976565
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-11-02 18:40:06 +0300

    list rendering from an array of strings

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let app = new Vue({
     data: {
         product: 'Socks',
         image: './assets/vmSocks-green.jpg',
-        inStock: true
+        inStock: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"]
     }
 });
